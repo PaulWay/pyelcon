336877b922def7aaec3c7e3dfc488874846e77bd
Paul Wayper <paulway@mabula.net>
2021-03-06 17:47:29 +1000

    Test pack and unpack of CAN message
    Signed-off-by: Paul Wayper <paulway@mabula.net>

--- tests/test_elconcharger.py
+++ tests/test_elconcharger.py
@@ -1,6 +1,8 @@
+from can import Message
+
 import unittest
 
-from elcon_frames import ElconCharger
+from elcon_frames import ElconCharger, elcon_charger_id, elcon_manager_id
 
 class ElconChargerTests(unittest.TestCase):
 
@@ -15,5 +17,28 @@ class ElconChargerTests(unittest.TestCase):
         ec = ElconCharger()
         self.assertTrue(
             ec.unpack_elcon_id(0x1806E5F4),
-            (0xE5, 0xF4)
+            (elcon_charger_id, elcon_manager_id)
         )
+
+    def test_unpack_status(self):
+        ec = ElconCharger()
+        msg = Message(arbitration_id=0x1806E5F4, data=b'\x01\x00\x00\x10\x08')
+        # Message is from the charger, can decode
+        self.assertTrue(ec.unpack_status(msg))
+        # Check message details
+        self.assertEqual(ec.voltage, 25.6)
+        self.assertEqual(ec.current, 1.6)
+        self.assertFalse(ec.hardware_failure)
+        self.assertFalse(ec.over_temperature)
+        self.assertFalse(ec.input_voltage)
+        self.assertTrue(ec.no_battery)
+        self.assertFalse(ec.timeout)
+
+        # Add tests of failure modes here
+
+    def test_pack_status(self):
+        ec = ElconCharger()
+        msg = ec.pack_command(51.2, 3.2, True)
+        self.assertIsInstance(msg, Message)
+        self.assertEqual(msg.arbitration_id, 0x1806E5F4)
+        self.assertEqual(msg.data, b'\x02\x00\x00\x20\x01')
