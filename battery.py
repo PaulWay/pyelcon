594f78c129b15f955c28485c63937fe06b00aa2f
Paul Wayper <paulway@mabula.net>
2021-03-06 15:51:17 +1000

    Add a module for simulating a battery and a charger.
    This module provides the `Battery` and `Charger` classes, that
    simulate the operation of a battery and charger respectively.
    
    The `Battery` is instantiated with a capacity (in amp-hours, as a
    float) and number of cells (as an integer).  It defaults to 50%
    charged.  It can be charged and discharged by applying amps for a
    number of seconds, and charging will also take the voltage that
    the battery is being charged at.  The voltage of the battery
    corresponds to its state of charge, using a cubic function that
    (roughly) simulates the charge/discharge curve of a lithium-ion
    battery.  It cannot be over-charged or over-discharged (limiting
    to fully charged or fully discharged).
    
    The `Charger` is instantiated with an instance of a battery given
    by the class above.  It can then be set to a given voltage and
    current, and told to charge the attached battery for a given number
    of seconds.  Charging is finished when the battery's voltage goes
    above the charger's set voltage - there is no 'constant voltage'
    phase of the discharge in this simulation.
    
    Signed-off-by: Paul Wayper <paulway@mabula.net>

---
+++ battery.py
@@ -0,0 +1,97 @@
+# battsim - simulate a Lithium Ion battery.
+# Written by Paul Wayper
+# Licensed under the GPL V3
+
+class Battery(object):
+    """
+    A simulated battery that can be charged and discharged using amp-hours,
+    and outputs its voltage
+    """
+    capacity = 10  # amp-hours
+    cells = 1  # number of cells
+    charge_rate = 1  # C
+    discharge_rate = 1  # C
+    minimum_voltage = 3.0  # volts
+    storage_voltage = 3.75  # volts
+    maximum_voltage = 4.2  # volts
+
+    def __init__(self, capacity, cells):
+        """
+        Set up a new battery, with capacity given in amp-hours.
+
+        The battery starts at 50% capacity, and the storage voltage.
+        """
+        self.capacity = capacity
+        self.charge_state = capacity / 2  # Start at half capacity
+        self.cells = cells
+
+    def charge(self, volts, amps, seconds):
+        """
+        Add amp-hours to the battery, by taking the given amps over the given
+        number of seconds.
+
+        Will not charge if the given voltage is less than the battery's
+        voltage.
+
+        No real attention to 'power' in watts or yet.
+        """
+        if self.voltage > volts:
+            return
+        self.charge_state += (amps * seconds) / 3600
+        if self.charge_state > self.capacity:
+            self.charge_state = self.capacity
+
+    def discharge(self, amps, seconds):
+        """
+        Subtract amp-hours from the battery, by producing the given amps over
+        the given number of seconds.
+        """
+        self.charge_state -= (amps * seconds) / 3600
+        if self.charge_state < 0:
+            self.charge_state = 0
+
+
+    @property
+    def voltage(self):
+        """
+        Output the current voltage of the battery, based on its state of
+        charge.
+
+        This uses a hand-tuned cubic formula to simulate the voltage curve of
+        a LiIon battery, with a flat but gradual increase in the centre and
+        steep drop and climb at either end of charging.  So I can't really
+        explain why these constants, but they seem to work.
+        """
+        vrange = self.maximum_voltage - self.minimum_voltage
+        frac_charge = (self.charge_state / self.capacity) - 0.5  # range -0.5 to 0.5
+        return (
+            (frac_charge ** 3 * 3.6 + frac_charge * 0.1 + 0.5)
+             * vrange + self.minimum_voltage
+        ) * self.cells
+
+    def __str__(self):
+        return f"{self.capacity}Ah {self.cells}S battery with {self.charge_state:.3f}Ah charge"
+
+
+class Charger(object):
+    """
+    A simulated charger, connected to a battery, that applies voltage and
+    current to a battery over time to charge it.
+    """
+    volts = 0
+    amps = 0
+
+    def __init__(self, battery):
+        self.battery = battery
+
+    def set(self, volts, amps):
+        self.volts = volts
+        self.amps = amps
+
+    def charge(self, seconds):
+        self.battery.charge(self.volts, self.amps, seconds)
+
+    @property
+    def is_finished(self):
+        return self.volts < self.battery.voltage
+
