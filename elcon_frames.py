27a5758a9d8997197b05f39f302109e355b40a2c
Paul Wayper <paulway@mabula.net>
2021-12-08 09:30:36 +1100

    Set flag bits with or, dummy
    Signed-off-by: Paul Wayper <paulway@mabula.net>

--- elcon_frames.py
+++ elcon_frames.py
@@ -111,22 +111,22 @@ class ElconUtils(object):
         msg.dlc = 5
         # Send a message from us (source) to the destination
         msg.arbitration_id = self.pack_elcon_id(self.our_id, pkt_dest)
+        flags = 0
         if pkt_dest == elcon_charger_id:
             # Message to charger
             flags = 1 if enable else 0
         else:
             # Message from charger to rest of world
-            flags = 0
             if self.hardware_failure:
-                flags &= 0x01
+                flags |= 0x01
             if self.over_temperature:
-                flags &= 0x02
+                flags |= 0x02
             if self.input_voltage:
-                flags &= 0x04
+                flags |= 0x04
             if self.no_battery:
-                flags &= 0x08
+                flags |= 0x08
             if self.timeout:
-                flags &= 0x10
+                flags |= 0x10
 
         msg.data = pack(">HHB", v, i, flags)
         msg.is_extended_id = True
