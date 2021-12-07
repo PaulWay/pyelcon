2a71f4d0283e6cef462aa20d33364a03cc89cb71
Paul Wayper <paulway@mabula.net>
2021-12-08 09:23:13 +1100

    Updating tests for src/dest order
    Signed-off-by: Paul Wayper <paulway@mabula.net>

--- tests/test_elconcharger.py
+++ tests/test_elconcharger.py
@@ -11,7 +11,7 @@ class ElconUtilsTests(unittest.TestCase):
     def test_pack_id(self):
         eu = ElconUtils(elcon_charger_id)
         self.assertEqual(
-            eu.pack_elcon_id(elcon_charger_id, elcon_manager_id),
+            eu.pack_elcon_id(elcon_manager_id, elcon_charger_id),
             magic_id
         )
 
@@ -19,14 +19,14 @@ class ElconUtilsTests(unittest.TestCase):
         eu = ElconUtils(elcon_charger_id)
         self.assertEqual(
             eu.unpack_elcon_id(magic_id),
-            (elcon_charger_id, elcon_manager_id)
+            (elcon_manager_id, elcon_charger_id)
         )
 
     def test_unpack_status(self):
         eu = ElconUtils(elcon_charger_id)
         msg = Message(arbitration_id=magic_id, data=b'\x01\x00\x00\x10\x08')
         # Message is from the charger, can decode
-        self.assertTrue(ec.unpack_status(msg))
+        self.assertTrue(eu.unpack_status(msg))
         # Check message details
         self.assertEqual(eu.voltage, 25.6)
         self.assertEqual(eu.current, 1.6)
@@ -39,8 +39,8 @@ class ElconUtilsTests(unittest.TestCase):
         # Add tests of failure modes here
 
     def test_pack_status(self):
-        eu = ElconUtils(elcon_charger_id)
-        msg = eu.pack_command(51.2, 3.2, True)
+        eu = ElconUtils(elcon_manager_id)
+        msg = eu.pack_command(elcon_charger_id, 51.2, 3.2, True)
         self.assertIsInstance(msg, Message)
-        self.assertEqual(msg.arbitration_id, 0x1806E5F4)
+        self.assertEqual(msg.arbitration_id, magic_id)
         self.assertEqual(msg.data, b'\x02\x00\x00\x20\x01')
