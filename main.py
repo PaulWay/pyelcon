049833981cb5346b9a2c237a01b5f55210b34560
Paul Wayper <paulway@mabula.net>
2021-12-21 08:45:21 +1100

    Simplify the objects we create
    Signed-off-by: Paul Wayper <paulway@mabula.net>

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-"""
-import simulator
 import asyncio
-cd = simulator.ChargerDriver(simulator.can0)
-cd.volts = 120
-cd.amps = 10
-cd.start()
-asyncio.run(cd.send_message())
-"""
+import can
+
+import battery
+import driver
+import simulator
 
 can0 = can.Bus('vcan0', bustype='socketcan')
-bat = Battery(capacity=10, cells=30)
-driver = ChargerDriver(can0)
+bat = battery.Battery(capacity=10, cells=30)
+driver = driver.ChargerDriver(can0)
 driver.volts = 120
 driver.amps = 10
 driver.start()
 
-sim = ElconCharger(can0, bat)
+sim = simulator.ElconCharger(can0, bat)
