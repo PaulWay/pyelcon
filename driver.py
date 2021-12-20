c7e8850c95a564aad41a9f5d313329e3eb211130
Paul Wayper <paulway@mabula.net>
2021-12-21 09:16:38 +1100

    Add command-line input to control driver
    This adds a new coroutine method that prompts the user for commands,
    interprets them, and sets the state of the driver appropriately.
    Commands include:
    
     * help, h, ? - help text
     * start, go - start sending messages
     * stop - stop sending messages
     * volts <x> - set the voltage to float(x)
     * amps <x> - set the amperage to float(x)
     * watts <x> - set the maximum watts to float(x)
     * finish, exit, quit, q - exit the driver
    
    Signed-off-by: Paul Wayper <paulway@mabula.net>

--- driver.py
+++ driver.py
@@ -1,7 +1,7 @@
 import asyncio
 import can
 
-from elcon_frames import (
+from utils import (
     ElconUtils, elcon_charger_id, elcon_manager_id, elcon_broadcast_id
 )
 
@@ -12,8 +12,8 @@ class ChargerDriver(object):
 
     Every `update_time` seconds, this will send a message on the CANBUS to
     an Elcon charger with the `volts` and `amps` to run at.  Messages will be
-    sent from when the 'start' method is called until the 'finish' method is
-    called.
+    sent from when the 'start' method is called until the 'stop' method is
+    called.  The 'finished' method will cause the driver to exit completely.
     """
     volts: float = 0.0
     amps: float = 0.0
@@ -26,6 +26,7 @@ class ChargerDriver(object):
         self.bus = bus
         self.utils = ElconUtils(our_id=elcon_manager_id)
         self.running = False
+        self.finished = False
 
     def _curb_amps_to_power(self):
         if (self.volts * self.amps) / self.efficiency_pct > self.max_watts:
@@ -33,24 +34,28 @@ class ChargerDriver(object):
 
     def start(self):
         self.running = True
-        # Start task here
+
+    def stop(self):
+        self.running = False
 
     def finish(self):
-        self.running = False
+        # Any shutdown
+        self.finished = True
 
     async def send_message(self):
         """
         Send the message to the charger.
         """
-        while self.running:
-            self._curb_amps_to_power()
-            msg = self.utils.pack_command(
-                elcon_charger_id, self.volts, self.amps, enable=True
-            )
-            if msg is not None:
-                self.bus.send(msg)
-                if self.verbose:
-                    print(f"Charger told to run at {self.volts:.2f}V {self.amps:.2f}A")
+        while not self.finished:
+            if self.running:
+                self._curb_amps_to_power()
+                msg = self.utils.pack_command(
+                    elcon_charger_id, self.volts, self.amps, enable=True
+                )
+                if msg is not None:
+                    self.bus.send(msg)
+                    if self.verbose:
+                        print(f"Charger told to run at {self.volts:.2f}V {self.amps:.2f}A")
             await asyncio.sleep(self.update_time)
 
     async def receive_status(self):
@@ -70,6 +75,48 @@ class ChargerDriver(object):
                 )
             else:
                 pass
+
+    def split_cmd_float(self, cmd):
+        cmd, val_s = cmd.split()
+        try:
+            return float(val_s)
+        except ValueError:
+            print(f"Didn't understand {val_s} as a float")
+            return None
+
+    async def read_command_line(self):
+        while not self.finished:
+            cmd = input("Cmd: ").strip().lower()
+
+            if cmd in ('help', 'h', '?'):
+                print("Help - commands we recognise:")
+                print("start    : start sending messages")
+                print("stop     : stop sending messages")
+                print("volts <x>: set output voltage to <x>")
+                print("amps <x> : set output amperage to <x>")
+                print("watts <x>: set maximum wattage to <x>")
+                print("quit     : finish operation and exit")
+            if cmd in ('start', 'go'):
+                self.start()
+            elif cmd in ('stop', ):
+                self.stop()
+            elif cmd in ('finish', 'exit', 'quit', 'q'):
+                self.finish()
+            elif cmd.startswith('volts'):
+                volts = self.split_cmd_float(cmd)
+                if volts is not None:
+                    self.volts = volts
+            elif cmd.startswith('amps'):
+                amps = self.split_cmd_float(cmd)
+                if amps is not None:
+                    self.amps = amps
+            elif cmd.startswith('watts'):
+                watts = self.split_cmd_float(cmd)
+                if watts is not None:
+                    self.max_watts = watts
+            else:
+                print(f"Unrecognised command '{cmd}'")
+
 
     async def main(self):
         """
